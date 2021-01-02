e6e8be6ce97de0bb5889de7bb5eb1034b564da20
Adam Kozdrowicz <akozdrowicz@gmail.com>
2021-01-02 17:55:42 -0500

    figured out phase 4

--- bomb/bomb.s
+++ bomb/bomb.s
@@ -418,49 +418,52 @@ Disassembly of section .text:
   400fc9:	48 83 c4 18          	add    $0x18,%rsp
   400fcd:	c3                   	retq   
 
+# arg1 in edi, rsp + 8
+# arg2 in eax, rsp + 12
+
 0000000000400fce <func4>:
-  400fce:	48 83 ec 08          	sub    $0x8,%rsp
-  400fd2:	89 d0                	mov    %edx,%eax
-  400fd4:	29 f0                	sub    %esi,%eax
-  400fd6:	89 c1                	mov    %eax,%ecx
-  400fd8:	c1 e9 1f             	shr    $0x1f,%ecx
-  400fdb:	01 c8                	add    %ecx,%eax
-  400fdd:	d1 f8                	sar    %eax
-  400fdf:	8d 0c 30             	lea    (%rax,%rsi,1),%ecx
-  400fe2:	39 f9                	cmp    %edi,%ecx
+  400fce:	48 83 ec 08          	sub    $0x8,%rsp # moving the rsp address back by 8
+  400fd2:	89 d0                	mov    %edx,%eax # first time around: 14 in edx, gets moved to eax
+  400fd4:	29 f0                	sub    %esi,%eax # first time around: 0 in esi, sub 0 from eax
+  400fd6:	89 c1                	mov    %eax,%ecx # 14
+  400fd8:	c1 e9 1f             	shr    $0x1f,%ecx shift 14 by 31 bits (so ecx is effectively 0)
+  400fdb:	01 c8                	add    %ecx,%eax # eax is still 14... seems like we want arg2 to be negative
+  400fdd:	d1 f8                	sar    %eax # eax is 7 (left shift by 1)
+  400fdf:	8d 0c 30             	lea    (%rax,%rsi,1),%ecx # eax is still 7
+  400fe2:	39 f9                	cmp    %edi,%ecx # edi: first arg, ecx: 7 the first time around
   400fe4:	7e 0c                	jle    400ff2 <func4+0x24>
   400fe6:	8d 51 ff             	lea    -0x1(%rcx),%edx
   400fe9:	e8 e0 ff ff ff       	callq  400fce <func4>
   400fee:	01 c0                	add    %eax,%eax
   400ff0:	eb 15                	jmp    401007 <func4+0x39>
-  400ff2:	b8 00 00 00 00       	mov    $0x0,%eax
-  400ff7:	39 f9                	cmp    %edi,%ecx
-  400ff9:	7d 0c                	jge    401007 <func4+0x39>
-  400ffb:	8d 71 01             	lea    0x1(%rcx),%esi
+  400ff2:	b8 00 00 00 00       	mov    $0x0,%eax # move 0 to eax (used to be 7)
+  400ff7:	39 f9                	cmp    %edi,%ecx # 7 and 7 in both registers
+  400ff9:	7d 0c                	jge    401007 <func4+0x39> # jump if greater than or equal (so jump happens since 7 = 7) -> (14 in edi > 7 in ecx, jump dont happen)... also we can insinuate from this code that we are looking for a number <=14. Now we can brute force by just testing combinations of (i,0) where <=0 i <= 14
+  400ffb:	8d 71 01             	lea    0x1(%rcx),%esi # 8 in esi, 7 in rcx () (looks like esi just gets incremented by 1)
   400ffe:	e8 cb ff ff ff       	callq  400fce <func4>
   401003:	8d 44 00 01          	lea    0x1(%rax,%rax,1),%eax
   401007:	48 83 c4 08          	add    $0x8,%rsp
   40100b:	c3                   	retq   
 
 000000000040100c <phase_4>:
-  40100c:	48 83 ec 18          	sub    $0x18,%rsp
-  401010:	48 8d 4c 24 0c       	lea    0xc(%rsp),%rcx
-  401015:	48 8d 54 24 08       	lea    0x8(%rsp),%rdx
-  40101a:	be cf 25 40 00       	mov    $0x4025cf,%esi
+  40100c:	48 83 ec 18          	sub    $0x18,%rsp # rsp address: 0x7ffe696dd650
+  401010:	48 8d 4c 24 0c       	lea    0xc(%rsp),%rcx # rcx: 0x7ffe696dd65c (rsp + 12)... no values in here just yet
+  401015:	48 8d 54 24 08       	lea    0x8(%rsp),%rdx # rdx: 0x7ffe696dd658 (rsp + 8)... no values in here just yet
+  40101a:	be cf 25 40 00       	mov    $0x4025cf,%esi # this is the string we're reading... "%d, %d"... so definitely two numbers
   40101f:	b8 00 00 00 00       	mov    $0x0,%eax
-  401024:	e8 c7 fb ff ff       	callq  400bf0 <__isoc99_sscanf@plt>
-  401029:	83 f8 02             	cmp    $0x2,%eax
-  40102c:	75 07                	jne    401035 <phase_4+0x29>
-  40102e:	83 7c 24 08 0e       	cmpl   $0xe,0x8(%rsp)
+  401024:	e8 c7 fb ff ff       	callq  400bf0 <__isoc99_sscanf@plt> # returns the number of items successfully matched
+  401029:	83 f8 02             	cmp    $0x2,%eax # eax must be 2 so that we don't trigger bomb. arg1 in rsp + 8. arg2 in rsp + 12
+  40102c:	75 07                	jne    401035 <phase_4+0x29> # don't want to jump
+  40102e:	83 7c 24 08 0e       	cmpl   $0xe,0x8(%rsp) # want to go here... "jump if below or equal 14" ... 0 in rsp + 8 cmpl only compares UNSIGNED, so first arg MUST be greater than zero
   401033:	76 05                	jbe    40103a <phase_4+0x2e>
   401035:	e8 00 04 00 00       	callq  40143a <explode_bomb>
-  40103a:	ba 0e 00 00 00       	mov    $0xe,%edx
-  40103f:	be 00 00 00 00       	mov    $0x0,%esi
-  401044:	8b 7c 24 08          	mov    0x8(%rsp),%edi
-  401048:	e8 81 ff ff ff       	callq  400fce <func4>
-  40104d:	85 c0                	test   %eax,%eax
-  40104f:	75 07                	jne    401058 <phase_4+0x4c>
-  401051:	83 7c 24 0c 00       	cmpl   $0x0,0xc(%rsp)
+  40103a:	ba 0e 00 00 00       	mov    $0xe,%edx # 14 in edx 
+  40103f:	be 00 00 00 00       	mov    $0x0,%esi # 0 in esi
+  401044:	8b 7c 24 08          	mov    0x8(%rsp),%edi # 7 in edi (arg1 in edi)
+  401048:	e8 81 ff ff ff       	callq  400fce <func4> 
+  40104d:	85 c0                	test   %eax,%eax # sets the zero flag when result of AND operation is zero (eax MUST be zero)
+  40104f:	75 07                	jne    401058 <phase_4+0x4c> # tests the zero flag and jumps if not set
+  401051:	83 7c 24 0c 00       	cmpl   $0x0,0xc(%rsp) # 0 passed in as first arg
   401056:	74 05                	je     40105d <phase_4+0x51>
   401058:	e8 dd 03 00 00       	callq  40143a <explode_bomb>
   40105d:	48 83 c4 18          	add    $0x18,%rsp
